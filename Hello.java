7f68119ef762039ee70638c9d6feb0d8419bf5a6
TEJASWINIRAMESH <tejaswiniramesh6777@gmail.com>
2024-11-08 01:07:35 +0530

    Add new modified

---
+++ Hello.java
@@ -0,0 +1,11 @@
+public class Hello {
+    public static void main(String[] args) {
+        int number = 10; 
+        System.out.println("The number is: " + number);
+        if (number % 2 == 0) {
+            System.out.println(number + " is even.");
+        } else {
+            System.out.println(number + " is odd.");
+        }
+    }
+}
